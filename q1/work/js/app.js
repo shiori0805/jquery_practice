38bf756d3c80facedea35ed4549c178e10346864
shiori <nyanko-12.11@outlook.jp>
2025-03-03 20:54:32 +0900

    update

--- q1/work/js/app.js
+++ q1/work/js/app.js
@@ -70,7 +70,7 @@ $(function () {
     //indexでq10のliの順番を取得し、クリックしたときに
     const index = $(this).index();
     //consoleにq11とliのリストを取得し、表示する
-    console.log($("#q11 li").eq(expand));
+    console.log($("#q11 li").eq(index));
     //q11、liを取得しそのリストにaddClassで"large-text"のクラス属性を追加し、cssで記述された"large-text"が実行される。
     $("#q11 li").eq(index).addClass("large-text")
   })
